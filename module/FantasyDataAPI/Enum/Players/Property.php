e17337562154d77ddb317bc1ee83df1cb395666e
Robert Johnson <robj@gridiron-guru.com>
2014-06-07 22:39:16 -0400

    Adding Players Enum

---
+++ module/FantasyDataAPI/Enum/Players/Property.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @link      https://github.com/gridiron-guru/FantasyDataAPI for the canonical source repository
+ * @copyright Copyright (c) 2014 Robert Gunnar Johnson Jr.
+ * @license   http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ * @package   FantasyDataAPI
+ */
+
+namespace FantasyDataAPI\Enum\Players;
+
+
+class Property
+{
+    const KEY_ACTIVE = 'Active';
+    const KEY_AGE = 'Age';
+    const KEY_AVERAGE_DRAFT_POSITION = 'AverageDraftPosition';
+    const KEY_BIRTH_DATE = 'BirthDate';
+    const KEY_BIRTH_DATE_STRING = 'BirthDateString';
+    const KEY_BYE_WEEK = 'ByeWeek';
+    const KEY_COLLEGE = 'College';
+    const KEY_COLLEGE_DRAFT_PICK = 'CollegeDraftPick';
+    const KEY_COLLEGE_DRAFT_ROUND = 'CollegeDraftRound';
+    const KEY_COLLEGE_DRAFT_TEAM = 'CollegeDraftTeam';
+    const KEY_COLLEGE_DRAFT_YEAR = 'CollegeDraftYear';
+    const KEY_CURRENT_TEAM = 'CurrentTeam';
+    const KEY_DEPTH_DISPLAY_ORDER = 'DepthDisplayOrder';
+    const KEY_DEPTH_ORDER = 'DepthOrder';
+    const KEY_DEPTH_POSITION = 'DepthPosition';
+    const KEY_DEPTH_POSITION_CATEGORY = 'DepthPositionCategory';
+    const KEY_EXPERIENCE = 'Experience';
+    const KEY_EXPERIENCE_STRING = 'ExperienceString';
+    const KEY_FANTASY_POSITION = 'FantasyPosition';
+    const KEY_FIRST_NAME = 'FirstName';
+    const KEY_HEIGHT = 'Height';
+    const KEY_HEIGHT_FEET = 'HeightFeet';
+    const KEY_HEIGHT_INCHES = 'HeightInches';
+    const KEY_INJURY_STATUS = 'InjuryStatus';
+    const KEY_IS_UNDRAFTED_FREE_AGENT = 'IsUndraftedFreeAgent';
+    const KEY_LAST_NAME = 'LastName';
+    const KEY_LATEST_NEWS = 'LatestNews';
+    const KEY_NAME = 'Name';
+    const KEY_NUMBER = 'Number';
+    const KEY_PHOTO_URL = 'PhotoUrl';
+    const KEY_PLAYER_ID = 'PlayerID';
+    const KEY_PLAYER_SEASON = 'PlayerSeason';
+    const KEY_POSITION = 'Position';
+    const KEY_POSITION_CATEGORY = 'PositionCategory';
+    const KEY_SHORT_NAME = 'ShortName';
+    const KEY_STATUS = 'Status';
+    const KEY_TEAM = 'Team';
+    const KEY_UPCOMING_GAME_OPPONENT = 'UpcomingGameOpponent';
+    const KEY_UPCOMING_GAME_WEEK = 'UpcomingGameWeek';
+    const KEY_WEIGHT = 'Weight';
+}
