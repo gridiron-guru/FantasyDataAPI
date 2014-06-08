509093886aefcfb3c5fc694cfd2013d2b58a5fa2
Robert Johnson <robj@gridiron-guru.com>
2014-06-07 22:22:31 -0400

    Adding PlayerNews Enum

---
+++ module/FantasyDataAPI/Enum/PlayerNews/Property.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @link      https://github.com/gridiron-guru/FantasyDataAPI for the canonical source repository
+ * @copyright Copyright (c) 2014 Robert Gunnar Johnson Jr.
+ * @license   http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ * @package   FantasyDataAPI
+ */
+
+namespace FantasyDataAPI\Enum\PlayerNews;
+
+
+class Property
+{
+    const KEY_CONTENT = 'Content';
+    const KEY_NEWS_ID = 'NewsID';
+    const KEY_PLAYER_ID = 'PlayerID';
+    const KEY_SOURCE = 'Source';
+    const KEY_TEAM = 'Team';
+    const KEY_TERMS_OF_USE = 'TermsOfUse';
+    const KEY_TITLE = 'Title';
+    const KEY_UPDATED = 'Updated';
+    const KEY_URL = 'Url';
+}
