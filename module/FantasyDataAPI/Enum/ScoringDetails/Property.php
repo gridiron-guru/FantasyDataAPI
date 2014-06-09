28bfc179fa0fbb4d3f5443fb2f08889006dce968
Robert Johnson <robj@gridiron-guru.com>
2014-06-08 22:52:12 -0400

    Adding ScoringDetails enum

---
+++ module/FantasyDataAPI/Enum/ScoringDetails/Property.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @link      https://github.com/gridiron-guru/FantasyDataAPI for the canonical source repository
+ * @copyright Copyright (c) 2014 Robert Gunnar Johnson Jr.
+ * @license   http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ * @package   FantasyDataAPI
+ */
+
+namespace FantasyDataAPI\Enum\ScoringDetails;
+
+class Property
+{
+    const KEY_GAME_KEY = 'GameKey';
+    const KEY_LENGTH = 'Length';
+    const KEY_PLAYER_GAME_ID = 'PlayerGameID';
+    const KEY_PLAYER_ID = 'PlayerID';
+    const KEY_SCORING_DETAIL_ID = 'ScoringDetailID';
+    const KEY_SCORING_TYPE = 'ScoringType';
+    const KEY_SEASON = 'Season';
+    const KEY_SEASON_TYPE = 'SeasonType';
+    const KEY_TEAM = 'Team';
+    const KEY_WEEK = 'Week';
+}
