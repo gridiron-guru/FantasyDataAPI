e20a3e10b8bd0e178c92944e9b9a5c390dd2c898
Robert Johnson <robj@gridiron-guru.com>
2014-06-11 21:54:51 -0400

    Adding FantasyDefenseSeason enum resource

---
+++ module/FantasyDataAPI/FantasyDefenseSeason/Property.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @link      https://github.com/gridiron-guru/FantasyDataAPI for the canonical source repository
+ * @copyright Copyright (c) 2014 Robert Gunnar Johnson Jr.
+ * @license   http://opensource.org/licenses/Apache-2.0
+ * @package   FantasyDataAPI
+ */
+
+namespace FantasyDataAPI\Enum\FantasyDefenseSeason;
+
+class Property
+{
+    const KEY_ASSISTED_TACKLES = 'AssistedTackles';
+    const KEY_BLOCKED_KICK_RETURN_TOUCHDOWNS = 'BlockedKickReturnTouchdowns';
+    const KEY_BLOCKED_KICK_RETURN_YARDS = 'BlockedKickReturnYards';
+    const KEY_BLOCKED_KICKS = 'BlockedKicks';
+    const KEY_DEFENSIVE_TOUCHDOWNS = 'DefensiveTouchdowns';
+    const KEY_FANTASY_POINTS = 'FantasyPoints';
+    const KEY_FANTASY_POINTS_ALLOWED = 'FantasyPointsAllowed';
+    const KEY_FIELD_GOAL_RETURN_TOUCHDOWNS = 'FieldGoalReturnTouchdowns';
+    const KEY_FIELD_GOAL_RETURN_YARDS = 'FieldGoalReturnYards';
+    const KEY_FUMBLE_RETURN_TOUCHDOWNS = 'FumbleReturnTouchdowns';
+    const KEY_FUMBLE_RETURN_YARDS = 'FumbleReturnYards';
+    const KEY_FUMBLES_FORCED = 'FumblesForced';
+    const KEY_FUMBLES_RECOVERED = 'FumblesRecovered';
+    const KEY_GAMES = 'Games';
+    const KEY_HUMIDITY = 'Humidity';
+    const KEY_INTERCEPTION_RETURN_TOUCHDOWNS = 'InterceptionReturnTouchdowns';
+    const KEY_INTERCEPTION_RETURN_YARDS = 'InterceptionReturnYards';
+    const KEY_INTERCEPTIONS = 'Interceptions';
+    const KEY_KICK_RETURN_LONG = 'KickReturnLong';
+    const KEY_KICK_RETURN_TOUCHDOWNS = 'KickReturnTouchdowns';
+    const KEY_KICK_RETURN_YARDS = 'KickReturnYards';
+    const KEY_KICK_RETURNS = 'KickReturns';
+    const KEY_KICKER_FANTASY_POINTS_ALLOWED = 'KickerFantasyPointsAllowed';
+    const KEY_PASSES_DEFENDED = 'PassesDefended';
+    const KEY_POINTS_ALLOWED = 'PointsAllowed';
+    const KEY_PUNT_RETURN_LONG = 'PuntReturnLong';
+    const KEY_PUNT_RETURN_TOUCHDOWNS = 'PuntReturnTouchdowns';
+    const KEY_PUNT_RETURN_YARDS = 'PuntReturnYards';
+    const KEY_PUNT_RETURNS = 'PuntReturns';
+    const KEY_QUARTERBACK_FANTASY_POINTS_ALLOWED = 'QuarterbackFantasyPointsAllowed';
+    const KEY_QUARTERBACK_HITS = 'QuarterbackHits';
+    const KEY_RUNNINGBACK_FANTASY_POINTS_ALLOWED = 'RunningbackFantasyPointsAllowed';
+    const KEY_SACK_YARDS = 'SackYards';
+    const KEY_SACKS = 'Sacks';
+    const KEY_SAFETIES = 'Safeties';
+    const KEY_SCORING_DETAILS = 'ScoringDetails';
+    const KEY_SEASON = 'Season';
+    const KEY_SEASON_TYPE = 'SeasonType';
+    const KEY_SOLO_TACKLES = 'SoloTackles';
+    const KEY_SPECIAL_TEAMS_TOUCHDOWNS = 'SpecialTeamsTouchdowns';
+    const KEY_STADIUM = 'Stadium';
+    const KEY_TACKLES_FOR_LOSS = 'TacklesForLoss';
+    const KEY_TEAM = 'Team';
+    const KEY_TEMPERATURE = 'Temperature';
+    const KEY_TIGHT_END_FANTASY_POINTS_ALLOWED = 'TightEndFantasyPointsAllowed';
+    const KEY_TOUCHDOWNS_SCORED = 'TouchdownsScored';
+    const KEY_WEEK = 'Week';
+    const KEY_WIDE_RECEIVER_FANTASY_POINTS_ALLOWED = 'WideReceiverFantasyPointsAllowed';
+    const KEY_WINDSPEED = 'WindSpeed';
+}
