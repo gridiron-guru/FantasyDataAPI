970b651e0bb0e035b4ead2aa6f3bc5a197040eaf
Robert Johnson <robj@gridiron-guru.com>
2014-06-18 19:00:35 -0400

    Adding BoxScore resource enum

---
+++ module/FantasyDataAPI/Enum/BoxScore/Property.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @link      https://github.com/gridiron-guru/FantasyDataAPI for the canonical source repository
+ * @copyright Copyright (c) 2014 Robert Gunnar Johnson Jr.
+ * @license   http://opensource.org/licenses/Apache-2.0
+ * @package   FantasyDataAPI
+ */
+
+namespace FantasyDataAPI\Enum\BoxScore;
+
+
+class Property
+{
+    const KEY_AWAY_DEFENSE = 'AwayDefense';
+    const KEY_AWAY_FANTASY_DEFENSE = 'AwayFantasyDefense';
+    const KEY_AWAY_KICK_PUNT_RETURNS = 'AwayKickPuntReturns';
+    const KEY_AWAY_KICKING = 'AwayKicking';
+    const KEY_AWAY_PASSING = 'AwayPassing';
+    const KEY_AWAY_PUNTING = 'AwayPunting';
+    const KEY_AWAY_RECEIVING = 'AwayReceiving';
+    const KEY_AWAY_RUSHING = 'AwayRushing';
+    const KEY_GAME = 'Game';
+    const KEY_HOME_DEFENSE = 'HomeDefense';
+    const KEY_HOME_FANTASY_DEFENSE = 'HomeFantasyDefense';
+    const KEY_HOME_KICK_PUNT_RETURNS = 'HomeKickPuntReturns';
+    const KEY_HOME_KICKING = 'HomeKicking';
+    const KEY_HOME_PASSING = 'HomePassing';
+    const KEY_HOME_PUNTING = 'HomePunting';
+    const KEY_HOME_RECEIVING = 'HomeReceiving';
+    const KEY_HOME_RUSHING = 'HomeRushing';
+    const KEY_SCORE = 'Score';
+    const KEY_SCORING_DETAILS = 'ScoringDetails';
+    const KEY_SCORING_PLAYS = 'ScoringPlays';
+}
