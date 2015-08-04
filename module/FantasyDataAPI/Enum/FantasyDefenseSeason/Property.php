5f2738207677ec5750a5cce202889d786e9e428d
Base5 Designs <dizzy@base5designs.co.uk>
2015-08-04 15:38:18 +0200

    FantasyDefenceBySeason ENUM Properties and Integration Tests to match v2 API

--- module/FantasyDataAPI/Enum/FantasyDefenseSeason/Property.php
+++ module/FantasyDataAPI/Enum/FantasyDefenseSeason/Property.php
@@ -10,56 +10,69 @@ namespace FantasyDataAPI\Enum\FantasyDefenseSeason;
 
 class Property
 {
+    const KEY_SCORING_DETAILS = 'ScoringDetails';
+    const KEY_SEASON_TYPE = 'SeasonType';
+    const KEY_SEASON = 'Season';
+    const KEY_TEAM = 'Team';
+    const KEY_POINTS_ALLOWED = 'PointsAllowed';
+    const KEY_TOUCHDOWNS_SCORED = 'TouchdownsScored';
+    const KEY_SOLO_TACKLES = 'SoloTackles';
     const KEY_ASSISTED_TACKLES = 'AssistedTackles';
-    const KEY_BLOCKED_KICK_RETURN_TOUCHDOWNS = 'BlockedKickReturnTouchdowns';
-    const KEY_BLOCKED_KICK_RETURN_YARDS = 'BlockedKickReturnYards';
-    const KEY_BLOCKED_KICKS = 'BlockedKicks';
-    const KEY_DEFENSIVE_TOUCHDOWNS = 'DefensiveTouchdowns';
-    const KEY_FANTASY_POINTS = 'FantasyPoints';
-    const KEY_FANTASY_POINTS_ALLOWED = 'FantasyPointsAllowed';
-    const KEY_FIELD_GOAL_RETURN_TOUCHDOWNS = 'FieldGoalReturnTouchdowns';
-    const KEY_FIELD_GOAL_RETURN_YARDS = 'FieldGoalReturnYards';
-    const KEY_FOURTH_DOWN_ATTEMPTS = 'FourthDownAttempts';
-    const KEY_FOURTH_DOWN_CONVERSIONS = 'FourthDownConversions';
-    const KEY_FUMBLE_RETURN_TOUCHDOWNS = 'FumbleReturnTouchdowns';
-    const KEY_FUMBLE_RETURN_YARDS = 'FumbleReturnYards';
+    const KEY_SACKS = 'Sacks';
+    const KEY_SACK_YARDS = 'SackYards';
+    const KEY_PASSES_DEFENDED = 'PassesDefended';
     const KEY_FUMBLES_FORCED = 'FumblesForced';
     const KEY_FUMBLES_RECOVERED = 'FumblesRecovered';
-    const KEY_GAMES = 'Games';
-    const KEY_HUMIDITY = 'Humidity';
-    const KEY_INTERCEPTION_RETURN_TOUCHDOWNS = 'InterceptionReturnTouchdowns';
-    const KEY_INTERCEPTION_RETURN_YARDS = 'InterceptionReturnYards';
+    const KEY_FUMBLE_RETURN_YARDS = 'FumbleReturnYards';
+    const KEY_FUMBLE_RETURN_TOUCHDOWNS = 'FumbleReturnTouchdowns';
     const KEY_INTERCEPTIONS = 'Interceptions';
-    const KEY_KICK_RETURN_LONG = 'KickReturnLong';
-    const KEY_KICK_RETURN_TOUCHDOWNS = 'KickReturnTouchdowns';
-    const KEY_KICK_RETURN_YARDS = 'KickReturnYards';
-    const KEY_KICK_RETURNS = 'KickReturns';
-    const KEY_KICKER_FANTASY_POINTS_ALLOWED = 'KickerFantasyPointsAllowed';
-    const KEY_PASSES_DEFENDED = 'PassesDefended';
-    const KEY_POINTS_ALLOWED = 'PointsAllowed';
-    const KEY_POINTS_ALLOWED_BY_DEFENSE_SPECIAL_TEAMS = 'PointsAllowedByDefenseSpecialTeams';
-    const KEY_PUNT_RETURN_LONG = 'PuntReturnLong';
-    const KEY_PUNT_RETURN_TOUCHDOWNS = 'PuntReturnTouchdowns';
-    const KEY_PUNT_RETURN_YARDS = 'PuntReturnYards';
-    const KEY_PUNT_RETURNS = 'PuntReturns';
-    const KEY_QUARTERBACK_FANTASY_POINTS_ALLOWED = 'QuarterbackFantasyPointsAllowed';
-    const KEY_QUARTERBACK_HITS = 'QuarterbackHits';
-    const KEY_RUNNINGBACK_FANTASY_POINTS_ALLOWED = 'RunningbackFantasyPointsAllowed';
-    const KEY_SACK_YARDS = 'SackYards';
-    const KEY_SACKS = 'Sacks';
+    const KEY_INTERCEPTION_RETURN_YARDS = 'InterceptionReturnYards';
+    const KEY_INTERCEPTION_RETURN_TOUCHDOWNS = 'InterceptionReturnTouchdowns';
+    const KEY_BLOCKED_KICKS = 'BlockedKicks';
     const KEY_SAFETIES = 'Safeties';
-    const KEY_SCORING_DETAILS = 'ScoringDetails';
-    const KEY_SEASON = 'Season';
-    const KEY_SEASON_TYPE = 'SeasonType';
-    const KEY_SOLO_TACKLES = 'SoloTackles';
-    const KEY_SPECIAL_TEAMS_TOUCHDOWNS = 'SpecialTeamsTouchdowns';
+    const KEY_PUNT_RETURNS = 'PuntReturns';
+    const KEY_PUNT_RETURN_YARDS = 'PuntReturnYards';
+    const KEY_PUNT_RETURN_TOUCHDOWNS = 'PuntReturnTouchdowns';
+    const KEY_PUNT_RETURN_LONG = 'PuntReturnLong';
+    const KEY_KICK_RETURNS = 'KickReturns';
+    const KEY_KICK_RETURN_YARDS = 'KickReturnYards';
+    const KEY_KICK_RETURN_TOUCHDOWNS = 'KickReturnTouchdowns';
+    const KEY_KICK_RETURN_LONG = 'KickReturnLong';
+    const KEY_BLOCKED_KICK_RETURN_TOUCHDOWNS = 'BlockedKickReturnTouchdowns';
+    const KEY_FIELD_GOAL_RETURN_TOUCHDOWNS = 'FieldGoalReturnTouchdowns';
+    const KEY_FANTASY_POINTS_ALLOWED = 'FantasyPointsAllowed';
+    const KEY_QUARTERBACK_FANTASY_POINTS_ALLOWED = 'QuarterbackFantasyPointsAllowed';
+    const KEY_RUNNINGBACK_FANTASY_POINTS_ALLOWED = 'RunningbackFantasyPointsAllowed';
+    const KEY_WIDE_RECEIVER_FANTASY_POINTS_ALLOWED = 'WideReceiverFantasyPointsAllowed';
+    const KEY_TIGHT_END_FANTASY_POINTS_ALLOWED = 'TightEndFantasyPointsAllowed';
+    const KEY_KICKER_FANTASY_POINTS_ALLOWED = 'KickerFantasyPointsAllowed';
+    const KEY_GAMES = 'Games';
+    const KEY_BLOCKED_KICK_RETURN_YARDS = 'BlockedKickReturnYards';
+    const KEY_FIELD_GOAL_RETURN_YARDS = 'FieldGoalReturnYards';
+    const KEY_QUARTERBACK_HITS = 'QuarterbackHits';
     const KEY_TACKLES_FOR_LOSS = 'TacklesForLoss';
-    const KEY_TEAM = 'Team';
+    const KEY_DEFENSIVE_TOUCHDOWNS = 'DefensiveTouchdowns';
+    const KEY_SPECIAL_TEAMS_TOUCHDOWNS = 'SpecialTeamsTouchdowns';
+    const KEY_FANTASY_POINTS = 'FantasyPoints';
     const KEY_TEMPERATURE = 'Temperature';
+    const KEY_HUMIDITY = 'Humidity';
+    const KEY_WINDSPEED = 'WindSpeed';
     const KEY_THIRD_DOWN_ATTEMPTS = 'ThirdDownAttempts';
     const KEY_THIRD_DOWN_CONVERSIONS = 'ThirdDownConversions';
-    const KEY_TIGHT_END_FANTASY_POINTS_ALLOWED = 'TightEndFantasyPointsAllowed';
-    const KEY_TOUCHDOWNS_SCORED = 'TouchdownsScored';
-    const KEY_WIDE_RECEIVER_FANTASY_POINTS_ALLOWED = 'WideReceiverFantasyPointsAllowed';
-    const KEY_WINDSPEED = 'WindSpeed';
+    const KEY_FOURTH_DOWN_ATTEMPTS = 'FourthDownAttempts';
+    const KEY_FOURTH_DOWN_CONVERSIONS = 'FourthDownConversions';
+    const KEY_POINTS_ALLOWED_BY_DEFENSE_SPECIAL_TEAMS = 'PointsAllowedByDefenseSpecialTeams';
+    //New in V2 Feed
+    const KEY_AUCTION_VALUE = 'AuctionValue';
+    const KEY_AUCTION_VALUE_PPR = 'AuctionValuePPR';
+
+
+
+
+
+
+
+
+
+
 }
