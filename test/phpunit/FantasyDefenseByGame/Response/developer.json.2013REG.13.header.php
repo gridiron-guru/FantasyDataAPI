c1352e2431353bc2b0f50fcd3e2cd46f34ee58e2
Base5 Designs <dizzy@base5designs.co.uk>
2015-08-04 15:32:22 +0200

    FantasyDefenceByGame: Updated Properties for v2 api updated header and body samples for phpunit updated Integration Tests

--- test/phpunit/FantasyDefenseByGame/Response/developer.json.2013REG.13.header.php
+++ test/phpunit/FantasyDefenseByGame/Response/developer.json.2013REG.13.header.php
@@ -9,13 +9,18 @@
 $headers = [];
 $headers[] = 'HTTP/1.1 200 OK';
 $headers[] = 'Cache-Control: must-revalidate, max-age=0, private';
-$headers[] = 'Content-Length: 37998';
+$headers[] = 'Content-Length: 45016';
 $headers[] = 'Content-Type: application/json; charset=utf-8';
-$headers[] = 'Expires: Fri, 30 May 2014 02:12:19 GMT';
-$headers[] = 'Last-Modified: Fri, 30 May 2014 02:08:29 GMT';
+$headers[] = 'Expires: Tue, 04 Aug 2015 13:26:02 GMT';
+$headers[] = 'Last-Modified: Tue, 04 Aug 2015 13:25:32 GMT';
 $headers[] = 'Vary: Accept,Accept-Charset';
 $headers[] = 'X-Powered-By: ASP.NET';
 $headers[] = 'X-Mashape-Billing: Call Limit=1';
-$headers[] = 'Date: Fri, 30 May 2014 02:12:19 GMT';
+$headers[] = 'Date: Tue, 04 Aug 2015 13:25:32 GMT';
 
 return $headers;
+
+
+
+
+
