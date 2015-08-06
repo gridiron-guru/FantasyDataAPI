fb1e9ba63659017893c2e7f88a5a2acec90e27ca
Base5 Designs <dizzy@base5designs.co.uk>
2015-08-06 14:10:35 +0200

    Unit Testing updates: FinalBoxScores: V2 uses a new URL Structure: http://api.nfldata.apiphany.com/nfl/v2/JSON/FinalBoxScores Modified Mock to account for new url structure
    Modified Unit Tests to account for new url format
    Removed: Unit Tests for Subscription key and type (no longer used in v2 api), References to Subscription

--- test/phpunit/FinalBoxScores/Response/Mock.php
+++ test/phpunit/FinalBoxScores/Response/Mock.php
@@ -18,8 +18,9 @@ class Mock extends Response
     public function __construct (RequestInterface $pRequest)
     {
         /** url parsing "formula" for resource */
-        list(, $subscription, $format) = explode( '/', $pRequest->getPath() );
-
+        list(, , , $format) = explode( '/', $pRequest->getPath() );
+        /** hardcode subscription so we can keep using the same filenames!*/
+        $subscription = 'developer';
         $file_partial = __DIR__ . '/' . implode('.', [$subscription, $format]);
 
         $headers = include($file_partial . '.header.php');
